f5115d48ccb57906c317c2dab502ba500bb65264
Dhawal Mahajan <dhawal.mahajan1@gmail.com>
2025-06-16 11:43:11 +0530

    format certain files asper prettier

--- Graphs/bfs-traversal.cpp
+++ Graphs/bfs-traversal.cpp
@@ -1,43 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<int> bfs(vector<vector<int>> &adj)
-{
-    // Code here
-    queue<int> q;
-    q.push(0);
-    vector<int> ans;
-    vector<bool> visited(adj.size(), 0);
-    visited[0] = 1;
-    int node;
-    while (!q.empty())
-    {
-        node = q.front();
-        q.pop();
-        ans.push_back(node);
-        for (int j = 0; j < adj[node].size(); j++)
-        {
-            if (!visited[adj[node][j]])
-            {
-                visited[adj[node][j]] = 1;
-                q.push(adj[node][j]);
-            }
-        }
+vector<int> bfs(vector<vector<int>> &adj) {
+  // Code here
+  queue<int> q;
+  q.push(0);
+  vector<int> ans;
+  vector<bool> visited(adj.size(), 0);
+  visited[0] = 1;
+  int node;
+  while (!q.empty()) {
+    node = q.front();
+    q.pop();
+    ans.push_back(node);
+    for (int j = 0; j < adj[node].size(); j++) {
+      if (!visited[adj[node][j]]) {
+        visited[adj[node][j]] = 1;
+        q.push(adj[node][j]);
+      }
     }
-    return ans;
+  }
+  return ans;
 }
-int main()
-{
-    vector<vector<int>> adj = {
-        {1, 2},
-        {0, 3},
-        {0, 4},
-        {1},
-        {2}};
-    vector<int> result = bfs(adj);
-    for (int i : result)
-    {
-        cout << i << " ";
-    }
-    return 0;
+int main() {
+  vector<vector<int>> adj = {{1, 2}, {0, 3}, {0, 4}, {1}, {2}};
+  vector<int> result = bfs(adj);
+  for (int i : result) {
+    cout << i << " ";
+  }
+  return 0;
 }
--- Graphs/detect-cycle-directed-graph.cpp
+++ Graphs/detect-cycle-directed-graph.cpp
@@ -1,54 +1,41 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-bool detectCycle(int node, vector<vector<int>> &adj, vector<bool> &path, vector<bool> &visited)
-{
-    visited[node] = true;
-    path[node] = true;
-    // look for adjacent node
-    for (int j = 0; j < adj[node].size(); j++)
-    {
-        int neighbor = adj[node][j];
-        // if node is already present in my path then cycle is present
-        if (path[neighbor] == true)
-            return true;
-        // If neighbour already visited then ignore it
-        if (!visited[neighbor] && detectCycle(neighbor, adj, path, visited))
-            return true;
-    }
-    path[node] = false; // backtrack
-    return false;
+bool detectCycle(int node, vector<vector<int>> &adj, vector<bool> &path,
+                 vector<bool> &visited) {
+  visited[node] = true;
+  path[node] = true;
+  // look for adjacent node
+  for (int j = 0; j < adj[node].size(); j++) {
+    int neighbor = adj[node][j];
+    // if node is already present in my path then cycle is present
+    if (path[neighbor] == true) return true;
+    // If neighbour already visited then ignore it
+    if (!visited[neighbor] && detectCycle(neighbor, adj, path, visited))
+      return true;
+  }
+  path[node] = false;  // backtrack
+  return false;
 }
-bool isCyclic(int V, vector<vector<int>> &edges)
-{
-    vector<vector<int>> adj(V);
-    vector<bool> path(V, 0);
-    vector<bool> visited(V, 0);
-    for (auto &edge : edges)
-    {
-        adj[edge[0]].push_back(edge[1]);
-    }
-    for (int i = 0; i < V; i++)
-    {
-        if (!visited[i] && detectCycle(i, adj, path, visited))
-            return true;
-    }
-    return false;
+bool isCyclic(int V, vector<vector<int>> &edges) {
+  vector<vector<int>> adj(V);
+  vector<bool> path(V, 0);
+  vector<bool> visited(V, 0);
+  for (auto &edge : edges) {
+    adj[edge[0]].push_back(edge[1]);
+  }
+  for (int i = 0; i < V; i++) {
+    if (!visited[i] && detectCycle(i, adj, path, visited)) return true;
+  }
+  return false;
 }
 
-int main()
-{
-    int V = 6;
-    vector<vector<int>> edges = {
-        {5, 2},
-        {5, 0},
-        {4, 0},
-        {4, 1},
-        {2, 3},
-        {3, 1}};
+int main() {
+  int V = 6;
+  vector<vector<int>> edges = {{5, 2}, {5, 0}, {4, 0}, {4, 1}, {2, 3}, {3, 1}};
 
-    bool result = isCyclic(V, edges);
-    cout << "Cycle present: " << (result ? "Yes" : "No") << endl;
+  bool result = isCyclic(V, edges);
+  cout << "Cycle present: " << (result ? "Yes" : "No") << endl;
 
-    return 0;
+  return 0;
 }
